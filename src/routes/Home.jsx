948f5c537227970fe6ba61d2216ec785d94a80c2
Guilherme <joseguilhermecosta@gmail.com>
2024-10-16 23:48:35 -0300

    Adicionando seta à Página Início - Parte 1

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Seta from '../assets/seta2.png'
 
 const Home = () => {
     return (
@@ -8,9 +9,12 @@ const Home = () => {
                 <h1 className="text-white font-bold text-5xl sm:text-7xl xl:text-8xl duration-500">BASKETBALL</h1>
                 <h1 className="text-white font-bold text-5xl sm:text-7xl xl:text-8xl duration-500">ACADEMY</h1>
                 <p className="text-white text-1xl mt-9 sm:mt-7 px-20 text-center duration-500">Academia de basquete dedicada a quem deseja se destacar<br></br> nas quadras. <span className="font-bold">Entre agora para o nosso time!</span></p>
-                <Link to='/contato' className="mt-14 rounded-sm relative border-2 border-white bg-transparent py-2.5 px-5 font-medium uppercase text-white transition-colors before:absolute before:left-0 before:top-0 before:-z-60 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-white before:transition-transform before:duration-300 before:content-[''] hover:text-neutral-800 before:hover:scale-x-100">
+                <Link to='/contato' className="mt-10 rounded-sm relative border-2 border-white bg-transparent py-2.5 px-5 font-medium uppercase text-white transition-colors before:absolute before:left-0 before:top-0 before:-z-60 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-white before:transition-transform before:duration-300 before:content-[''] hover:text-neutral-800 before:hover:scale-x-100">
                     <button className="font-semibold relative z-60">Inscrever-se</button>
                 </Link>
+                <button className="absolute bottom-10 animate-bounce">
+                    <img src={Seta} className="w-8 rotate-90" />
+                </button>
             </div>
         </>
     )
