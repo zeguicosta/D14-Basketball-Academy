8327d155e3c7dc894ec903d65dc138d3d88e9011
Guilherme <joseguilhermecosta@gmail.com>
2024-10-21 12:29:00 -0300

    Adicionando Seção 4 da página inicial

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,6 +4,9 @@ import Equipe from '../assets/equipe.png'
 import Especializacao from '../assets/especializacao.png'
 import Filosofia from '../assets/filosofia.png'
 import VideoCamp from '../assets/campvideo.mp4'
+import Bola from '../assets/bola.png'
+import Legado from '../assets/legado.png'
+import Peso from '../assets/peso.png'
 
 const Home = () => {
     return (
@@ -14,32 +17,32 @@ const Home = () => {
                 <h1 className="text-white font-bold text-5xl sm:text-7xl xl:text-8xl duration-500">BASKETBALL</h1>
                 <h1 className="text-white font-bold text-5xl sm:text-7xl xl:text-8xl duration-500">ACADEMY</h1>
                 <p className="text-white text-1xl mt-9 sm:mt-7 px-20 text-center duration-500">Academia de basquete dedicada a quem deseja se destacar<br></br> nas quadras. <span className="font-bold">Entre agora para o nosso time!</span></p>
-                <Link to='/contato' className="mt-10 rounded-sm relative border-2 border-white bg-transparent py-2.5 px-5 font-medium uppercase text-white transition-colors before:absolute before:left-0 before:top-0 before:-z-56 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-white before:transition-transform before:duration-300 before:content-[''] hover:text-neutral-800 before:hover:scale-x-100">
-                    <button className="font-semibold relative z-55">Inscrever-se</button>
+                <Link to='/contato' className="mt-10 rounded-sm relative border-2 border-white bg-white py-2.5 px-8 font-medium uppercase text-neutral-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-56 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-customGreen before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 shadow-md shadow-white/40">
+                    <button className="font-semibold relative z-55">Participar</button>
                 </Link>
                 <button onClick={() => { 
-                    document.getElementById('sobre-nos').scrollIntoView({behavior: 'smooth'});
+                    document.getElementById('equipe').scrollIntoView({behavior: 'smooth'});
                     }} 
-                    className="absolute bottom-10 animate-bounce">
-                    <img src={Seta} className="w-8 rotate-90" />
+                    className="absolute bottom-10 animate-bounce rounded-full shadow-md shadow-white/40 hover:shadow-lg">
+                    <img src={Seta} className="w-8 rotate-90 hover:scale-110 duration-200" />
                 </button>
             </div>
 
-            {/* Sobre Nós */}
-            <div id="sobre-nos" className="bg-neutral-100"></div>
+            {/* Equipe */}
+            <div id="equipe" className="bg-neutral-100"></div>
             <div className="bg-neutral-100 h-auto min-h-fit flex flex-wrap justify-center px-8 py-32 items-center gap-6 text-center xl:text-left">
                 <div className="px-8">
-                    <h1 className="font-bold text-neutral-800 text-5xl mb-5 block1 animate-appearleft">Conheça a</h1>
-                    <h1 className="font-bold text-neutral-800 text-5xl block1 animate-appearleft">Nossa <span className="text-customGreen">Equipe</span></h1>
+                    <h1 className="font-bold text-neutral-800 text-5xl mb-5 ">Conheça a</h1>
+                    <h1 className="font-bold text-neutral-800 text-5xl ">Nossa <span className="text-customGreen">Equipe</span></h1>
 
-                    <p className="mt-8 text-neutral-800 block1 animate-appearleft">Descubra a história de cada um dos integrantes da D14.</p>
+                    <p className="mt-8 text-neutral-800">Descubra a história de cada um dos integrantes da D14.</p>
 
                     <Link to='/sobre' >
-                        <button className="bg-neutral-800 rounded-sm text-white font-semibold px-10 py-3 mt-8 shadow-md shadow-neutral-800/30 duration-200 hover:bg-customGreen block1 animate-appearleft">Conhecer</button>
+                        <button className="bg-customGreen rounded-sm text-white font-semibold px-10 py-3 mt-8 shadow-md shadow-customGreen/30 duration-150 hover:scale-105">Conhecer</button>
                     </Link>
 
                     <div className="flex flex-wrap text-neutral-800 gap-6 justify-center">
-                        <Link className="mt-12 bg-white text-left shadow-lg h-auto w-72 px-10 py-11 rounded-md block1 animate-appearleft hover:bg-neutral-200 duration-200 cursor-pointer hover:invert" to='/sobre' >
+                        <Link className="mt-12 bg-white text-left shadow-lg h-auto w-80 px-10 py-11 rounded-md duration-200 cursor-pointer hover:shadow-xl hover:translate-y-1" to='/sobre' >
                             <div>
                                 <img className="w-8" src={Especializacao} />
                                 <h3 className="font-bold text-[22px] py-4">Especialização</h3>
@@ -47,7 +50,7 @@ const Home = () => {
                             </div>
                         </Link>
 
-                        <Link className="mt-12 bg-white text-left shadow-lg h-auto w-72 px-10 py-11 rounded-md block1 animate-appearleft hover:bg-neutral-200 duration-200 cursor-pointer hover:invert" to='/sobre' >
+                        <Link className="mt-12 bg-white text-left shadow-lg h-auto w-80 px-10 py-11 rounded-md  duration-200 cursor-pointer hover:shadow-xl hover:translate-y-1" to='/sobre' >
                             <div>
                                 <img className="w-8" src={Filosofia} />
                                 <h3 className="font-bold text-[22px] py-4">Filosofia</h3>
@@ -57,34 +60,70 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="px-8 mt-10 xl:mt-0 block2 animate-appearright">
+                <div className="px-8 mt-10 xl:mt-0">
                     <img className="shadow-md w-auto" src={Equipe} />
                 </div>
             </div>
 
             {/* Camps */}
             <div className="h-auto bg-custom-gradient items-center justify-center text-center flex flex-col gap-10 py-32">
-                <h1 className="text-5xl font-bold text-white block1 animate-appearleft">Participe dos Nossos <span className="text-customGreen">Camps</span></h1>
-                <p className="text-white px-10 2xl:px-96 w-auto block1 animate-appearleft">Os camps são eventos periódicos que contam com a presença de treinadores nacionais e internacionais, com vasta experiência, para oferecer um aprendizado único aos nossos alunos.</p>
+                <h1 className="text-5xl font-bold text-white">Participe dos Nossos <span className="text-customGreen">Camps</span></h1>
+                <p className="text-white px-10 2xl:px-96 w-auto">Os camps são eventos periódicos que contam com a presença de treinadores nacionais e internacionais, com vasta experiência, para oferecer um aprendizado único aos nossos alunos.</p>
 
-                <div className="flex gap-6 block1 animate-appearleft">
+                <div className="flex gap-6">
                     <Link to='/treinamentos'>
-                        <button className="bg-white text-neutral-800 font-semibold rounded-sm px-8 py-3 duration-200 hover:bg-customGreen hover:text-white">
-                            Saiba Mais
+                        <button className="bg-customGreen shadow-customGreen/30 shadow-md text-white font-semibold rounded-sm px-8 py-3 duration-150 hover:scale-105">
+                            Ver Treinos
                         </button>
                     </Link>
 
-                    <Link to='/contato' className="rounded-sm relative border-2 border-white bg-transparent py-2.5 px-8 font-medium uppercase text-white transition-colors before:absolute before:left-0 before:top-0 before:-z-56 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-white before:transition-transform before:duration-300 before:content-[''] hover:text-neutral-800 before:hover:scale-x-100">
+                    <Link to='/contato' className="rounded-sm relative border-2 border-white bg-transparent py-2.5 px-8 font-medium uppercase text-white transition-colors before:absolute before:left-0 before:top-0 before:-z-56 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-customGreen before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100">
                         <button className="font-semibold relative z-56">
                             Participar
                         </button>
                     </Link>
                 </div>
 
-                <div className="block1 animate-appearleft">
+                <div>
                     <video className="w-full lg:w-[1000px]" controls>
                         <source src={VideoCamp} type="video/mp4" />
                     </video>
+                </div>
+            </div>
+
+            {/* Sobre Nós */}
+            <div className="bg-neutral-100 h-auto min-h-fit flex flex-col justify-center px-8 py-32 items-center gap-6 text-center xl:text-left text-neutral-800">
+                <h1 className="font-bold text-5xl"><span className="text-customGreen">Sobre</span> Nós</h1>
+                <p>Entenda a nossa origem e o que nos motiva a transformar o desempenho dos alunos.</p>
+
+                <Link to='/sobre' >
+                    <button className="bg-customGreen rounded-sm text-white font-semibold px-10 py-3 shadow-md shadow-customGreen/30 duration-150 hover:scale-105">Saiba Mais</button>
+                </Link>
+
+                <div className="flex flex-wrap text-neutral-800 gap-6 justify-center">
+                    <Link className="mt-12 bg-white text-left shadow-lg h-auto w-[350px] px-10 py-11 rounded-md duration-200 cursor-pointer hover:shadow-xl hover:translate-y-1" to='/sobre' >
+                        <div>
+                            <img className="w-8" src={Bola} />
+                            <h3 className="font-bold text-[22px] py-4">História</h3>
+                            <p>Conheça como a D14 surgiu, com o propósito de criar uma academia que vai além do esporte, promovendo desenvolvimento pessoal e valores essenciais para seus atletas.</p>
+                        </div>
+                    </Link>
+
+                    <Link className="mt-8 bg-white text-left shadow-lg h-auto w-[350px] px-10 py-11 rounded-md duration-200 cursor-pointer hover:shadow-xl hover:translate-y-1" to='/sobre' >
+                        <div>
+                            <img className="w-8" src={Legado} />
+                            <h3 className="font-bold text-[22px] py-4">Legado</h3>
+                            <p>Veja como a D14 Basketball Academy  está construindo um legado sólido ao formar atletas talentosos e comprometidos. Desde sua fundação, a D14 ​já ​preparou 23 atletas que ​seguiram seus estudos nos ​Estados Unidos.</p>
+                        </div>
+                    </Link>
+
+                    <Link className="mt-12 bg-white text-left shadow-lg h-auto w-[350px] px-10 py-11 rounded-md duration-200 cursor-pointer hover:shadow-xl hover:translate-y-1" to='/treinamentos' >
+                        <div>
+                            <img className="w-8" src={Peso} />
+                            <h3 className="font-bold text-[22px] py-4">Treinamento</h3>
+                            <p>Saiba mais sobre nossos 3 módulos de treinamento: coletivo, personalizado e os camps, criados por treinadores experientes, nacionais e internacionais, para garantir uma experiência de alto nível a todos os atletas.</p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </>
