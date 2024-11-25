8f72a7157c13b2ab8f8f9c278a777a7842fa2e5f
Guilherme <joseguilhermecosta@gmail.com>
2024-11-25 20:12:42 -0300

    Atualizando

--- src/components/StickyCards.jsx
+++ src/components/StickyCards.jsx
@@ -76,7 +76,7 @@ const Card = ({ position, card, scrollYProgress, isOddCard }) => {
             </h3>
           )}
           {card.description && (
-            <p className="mb-8 max-w-2xl text-center text-lg md:text-lg">
+            <p className="mb-8 max-w-2xl text-center text-md md:text-base">
               {card.description}
             </p>
           )}
