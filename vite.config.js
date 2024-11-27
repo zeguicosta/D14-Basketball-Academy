75b4531aa939a21f6c3a305496237c76b5fc3af3
Guilherme <joseguilhermecosta@gmail.com>
2024-11-27 13:08:12 -0300

    Tentando corrigir erro de MIME

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/D14-Basketball-Academy/", // Substitua pelo nome do seu repositório
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
 })
