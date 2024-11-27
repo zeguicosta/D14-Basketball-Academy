a0b500e48b3830fcbae51074aed2d162d1dddda7
Guilherme <joseguilhermecosta@gmail.com>
2024-11-27 13:29:07 -0300

    Atualizando arquivo vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Substitua pelo nome do seu repositório
+  base: "./",
   build: {
     rollupOptions: {
       output: {
