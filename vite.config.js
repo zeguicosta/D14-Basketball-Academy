d564b9b5a70ca163b925cb4ed2572e706e6f808e
Guilherme <joseguilhermecosta@gmail.com>
2024-11-27 13:14:49 -0300

    Adicionando arquivo headers

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/D14-Basketball-Academy/", // Substitua pelo nome do seu repositório
+  base: "./", // Substitua pelo nome do seu repositório
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
+        manualChunks: undefined
       }
     }
   }
